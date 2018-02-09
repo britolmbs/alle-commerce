c36a67ed33b027a636ea174a36b65188e2a15f20
Lucas Brito <britolmbs@hotmail.com>
2018-02-09 19:53:34 -0200

    login

--- resposta.php
+++ resposta.php
@@ -1,3 +1,9 @@
+<?php 
+include"header.php";
+ ?>
+
+
+
 <?php
 session_start();
 include "conexao.php";
@@ -15,10 +21,18 @@ if($result->num_rows>0){
 	while($row = $result->fetch_assoc()){
 		?>
 
+<section>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo "$row['nome']";?></h5>
+    <h6 class="card-subtitle mb-2 text-muted">Seja Bem Vindo ao All E-Commerce</h6>
+    <p class="card-text"></p>
+    <a href="entrega.php" class="card-link">Mudar Local de entrega</a>
+    <a href="index.php" class="card-link">Compras</a>
+  </div>
+</div>
 
-
-
-
+</section>
 		<?php 
 	}
 
