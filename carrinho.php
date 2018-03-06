1aede1a7ff540a4a3f7d4ffbeb138c935bae443a
Lucas brito <britolmbs@hotmail.com>
2018-03-05 21:13:18 -0300

    carrinho

--- carrinho.php
+++ carrinho.php
@@ -1,39 +1,31 @@
-<?php 
-include "header.php";
- ?>
-
-
-<?php 
-
-session_start();
-	if($_GET['acao'] == 'add' && isset($_GET['id'])){ 
-				$_SESSION['carrinho'] = $_GET['nome']; 		
-		}
-		/*
-if(isset($_GET['acao']) && in_array($_GET['acao'], array('add', 'del', 'up'))) {
-		
+<?php
+	include "header.php";
+	include "car.php";
+	include "product.php";
 	
+	$pdoConnection = require_once "connection.php";
+
+	if(isset($_GET['acao']) && in_array($_GET['acao'], array('add', 'del', 'up'))) {
+		
+		if($_GET['acao'] == 'add' && isset($_GET['id']) && preg_match("/^[0-9]+$/", $_GET['id'])){ 
+			addCart($_GET['id'], 1);			
+		}
 		if($_GET['acao'] == 'del' && isset($_GET['id']) && preg_match("/^[0-9]+$/", $_GET['id'])){ 
 			deleteCart($_GET['id']);
 		}
 		if($_GET['acao'] == 'up'){ 
-			if(isset($_SESSION['carrinho'][$id])){ 
-		if($quantity > 0) {
-			$_SESSION['carrinho'][$id] = $quantity;
-		} else {
-		 	deleteCart($id);
-		}
-	}
+			if(isset($_POST['prod']) && is_array($_POST['prod'])){ 
+				foreach($_POST['prod'] as $id => $qtd){
+						updateCart($id, $qtd);
+				}
+			}
 		} 
-		header('location: carrinho.php');
+		//header('location: carrinho.php');
 	}
-*/
-
- ?>
-
-
-<section>
-	
+	$resultsCarts = getContentCart($pdoConnection);
+	$totalCarts  = getTotalCart($pdoConnection);
+?>
+<body>
 	<div class="container">
 		<div class="card mt-5">
 			 <div class="card-body">
@@ -42,6 +34,9 @@ if(isset($_GET['acao']) && in_array($_GET['acao'], array('add', 'del', 'up'))) {
 	    	</div>
 		</div>
 
+
+
+		<?php if($resultsCarts) : ?>
 			<form action="carrinho.php?acao=up" method="post">
 			<table class="table table-strip">
 				<thead>
@@ -55,20 +50,20 @@ if(isset($_GET['acao']) && in_array($_GET['acao'], array('add', 'del', 'up'))) {
 					</tr>				
 				</thead>
 				<tbody>
-
+				  <?php foreach($resultsCarts as $result) : ?>
 					<tr>
 						
-						<td><?php echo $_SESSION['carrinho'];?></td>
-
+						<td><?php echo $result['name']?></td>
 						<td>
 							<input type="text" name="prod[<?php echo $result['id']?>]" value="<?php echo $result['quantity']?>" size="1" />
 														
 							</td>
-						<td>R$<?php echo number_format($result['price'], 2, ',', '.')?></td>
-						<td>R$<?php echo number_format($result['subtotal'], 2, ',', '.')?></td>
+						<td>R$<?php echo $result['price']?></td>
+						<td>R$<?php echo $result['subtotal']?></td>
 						<td><a href="carrinho.php?acao=del&id=<?php echo $result['id']?>" class="btn btn-danger">Remover</a></td>
 						
 					</tr>
+				<?php endforeach;?>
 				 <tr>
 				 	<td colspan="3" class="text-right"><b>Total: </b></td>
 				 	<td>R$<?php echo number_format($totalCarts, 2, ',', '.')?></td>
@@ -79,18 +74,15 @@ if(isset($_GET['acao']) && in_array($_GET['acao'], array('add', 'del', 'up'))) {
 			</table>
 
 			<a class="btn btn-info" href="index.php">Continuar Comprando</a>
-			<button class="btn btn-primary" type="submit">Atualizar Carrinho</button>
+			<button class="btn btn-secondary " type="submit">Atualizar Carrinho</button>
 
 			</form>
-
+	<?php endif?>
 		
 	</div>
-</section>
 
-
-
-
-
- <?php
-  include "footer.php"; 
+</body>
+	
+<?php
+ include "footer.php";
  ?>
---
+++ product.php
@@ -0,0 +1,13 @@
+<?php 
+function getProducts($pdo){
+	$sql = "SELECT *  FROM produto";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute();
+	return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+function getProductsByIds($pdo, $ids) {
+	$sql = "SELECT * FROM produto WHERE id IN (".$ids.")";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute();
+	return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
